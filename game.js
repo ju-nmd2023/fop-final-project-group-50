5d8d2fc24043d43d5e637e22ed3ce2b95b3edf5f
filiphalonen <157317694+filiphalonen@users.noreply.github.com>
2024-10-10 14:40:40 +0200

    added more obstacles
    added another array of bigger obstacles

--- game.js
+++ game.js
@@ -7,8 +7,9 @@ let gravity = 0.8; // Gravity pulling the character down
 let velocityY = 0; // Vertical velocity of the character
 let isJumping = false; // To track if the character is in the air
 
-// Define an array to store multiple obstacles
-let obstacles = [];
+// Define arrays to store multiple obstacles
+let obstaclesLow = [];
+let obstaclesHigh = []; // New array for high obstacles
 
 function setup() {
   createCanvas(1420, 700);
@@ -25,14 +26,25 @@ function setup() {
   // Create an instance of the Character class, passing the character object
   myCharacter = new Character(character);
 
-  // Spawn multiple obstacles with different positions and speeds
+  // Spawn multiple low obstacles with different positions and speeds
   for (let i = 0; i < 3; i++) {
-    obstacles.push({
+    obstaclesLow.push({
       x: 1420, // Start each obstacle off the screen
       y: 620, // Place at the same height as the character
       width: 50, // Width of the obstacle
       height: 50, // Height of the obstacle
       speed: random(5, 10, 15), // Random speed for each obstacle
+    });
+  }
+
+  // Spawn multiple high obstacles with different positions and speeds
+  for (let i = 0; i < 2; i++) {
+    obstaclesHigh.push({
+      x: 1420, // Start each obstacle off the screen
+      y: 570, // Higher position for these obstacles
+      width: 30, // Width of the obstacle
+      height: 100, // Height of the obstacle
+      speed: random(5, 10), // Random speed for each obstacle
     });
   }
 }
@@ -72,10 +84,16 @@ function draw() {
   // Draw the character
   myCharacter.draw();
 
-  // Update and draw all obstacles
-  for (let i = 0; i < obstacles.length; i++) {
-    updateObstacle(obstacles[i]);
-    drawObstacle(obstacles[i]);
+  // Update and draw all low obstacles
+  for (let i = 0; i < obstaclesLow.length; i++) {
+    updateObstacle(obstaclesLow[i]);
+    drawObstacle(obstaclesLow[i]);
+  }
+
+  // Update and draw all high obstacles
+  for (let i = 0; i < obstaclesHigh.length; i++) {
+    updateObstacle(obstaclesHigh[i]);
+    drawObstacle(obstaclesHigh[i]);
   }
 }
 
